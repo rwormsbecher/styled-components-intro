2471bc132aa41c5eb61039e1e1231d83f5273f73
Rodney Wormsbecher <rodney22@live.nl>
2023-03-10 17:42:46 +0100

    starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./stylesheets/styles.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-left-section">
+                    <img src="/assets/chanel_perfume.png" alt="plants and perfume" />
+                </div>
+                <div className="card-right-section">
+                    <div>
+                        <h1>Perfume</h1>
+                        <h2>
+                            Gabrielle
+                            <br /> Essence Eau
+                            <br /> De Parfum
+                        </h2>
+                        <p>
+                            A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
+                            for the House of Chanel.
+                        </p>
+
+                        <div className="price-container">
+                            <h3>&euro;149,99</h3>
+                            <span>&euro;469,99</span>
+                        </div>
+                    </div>
+
+                    <div className="button-container">
+                        <button>Add to cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
