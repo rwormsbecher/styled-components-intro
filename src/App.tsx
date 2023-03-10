9278e830361768cc45ef8a9f1a2dd6b8c6f79042
Rodney Wormsbecher <rodney22@live.nl>
2023-03-10 19:23:44 +0100

    extract card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
+import { CardContainer, CardLeftSide, CardRightSide } from "./components/ui/Card";
 import { SpacedHeader } from "./components/ui/SpacedHeader";
 import "./stylesheets/styles.scss";
 
 function App() {
     return (
         <div className="container">
-            <div className="card">
-                <div className="card-left-section">
-                    <img src="/assets/chanel_perfume.png" alt="plants and perfume" />
-                </div>
-                <div className="card-right-section">
-                    <div>
-                        <SpacedHeader text="perfume" />
-                        <h2>
-                            Gabrielle
-                            <br /> Essence Eau
-                            <br /> De Parfum
-                        </h2>
-                        <p>
-                            A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
-                            for the House of Chanel.
-                        </p>
+            <CardContainer>
+                <>
+                    <CardLeftSide>
+                        <img src="/assets/chanel_perfume.png" alt="plants and perfume" />
+                    </CardLeftSide>
+                    <CardRightSide>
+                        <>
+                            <div>
+                                <SpacedHeader text="perfume" />
+                                <h2>
+                                    Gabrielle
+                                    <br /> Essence Eau
+                                    <br /> De Parfum
+                                </h2>
+                                <p>
+                                    A floral, solar and voluptuous interpretation composed by Olivier Polge,
+                                    Perfumer-Creator for the House of Chanel.
+                                </p>
 
-                        <div className="price-container">
-                            <h3>&euro;149,99</h3>
-                            <span>&euro;469,99</span>
-                        </div>
-                    </div>
+                                <div className="price-container">
+                                    <h3>&euro;149,99</h3>
+                                    <span>&euro;469,99</span>
+                                </div>
+                            </div>
 
-                    <div className="button-container">
-                        <button>Add to cart</button>
-                    </div>
-                </div>
-            </div>
+                            <div className="button-container">
+                                <button>Add to cart</button>
+                            </div>
+                        </>
+                    </CardRightSide>
+                </>
+            </CardContainer>
         </div>
     );
 }
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardContainerUi = styled.div`
+    display: flex;
+    width: 600px;
+    height: 450px;
+    background-color: white;
+    border-radius: 1.5em;
+`;
+
+const CardSide = styled.div`
+    display: flex;
+    width: 50%;
+    height: 100%;
+`;
+
+const CardLeft = styled(CardSide)`
+    border-radius: 1.5em 0 0 1.5em;
+
+    img {
+        border-radius: 1.5em 0 0 1.5em;
+        width: 100%;
+    }
+`;
+
+const CardRight = styled(CardSide)`
+    border-radius: 1.5em 0 0 1.5em;
+    padding: 1.5em;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 0 1.5em 1.5em 0;
+
+    h2 {
+        font-family: "Merriweather", serif;
+        font-size: 1.75rem;
+        font-weight: bold;
+        color: $header-color;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        line-height: 1.5rem;
+        margin-bottom: 1rem;
+    }
+
+    .price-container {
+        width: 100%;
+        height: 32px;
+        display: flex;
+        align-items: center;
+
+        h3 {
+            font-size: 24px;
+            color: $primary-color;
+            margin-right: 1.5rem;
+        }
+
+        span {
+            text-decoration: line-through;
+        }
+    }
+`;
+
+type childrenOnlyProps = {
+    children?: JSX.Element;
+};
+
+export const CardContainer: React.FC<childrenOnlyProps> = ({ children }) => {
+    return <CardContainerUi>{children}</CardContainerUi>;
+};
+
+export const CardLeftSide: React.FC<childrenOnlyProps> = ({ children }) => {
+    return <CardLeft>{children}</CardLeft>;
+};
+
+export const CardRightSide: React.FC<childrenOnlyProps> = ({ children }) => {
+    return <CardRight>{children}</CardRight>;
+};
